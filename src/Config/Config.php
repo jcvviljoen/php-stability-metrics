e5aa7b94c495677041275a0c7d376c7e64f523a0
Jason Viljoen <102794323+jcviljoen@users.noreply.github.com>
2024-07-31 20:19:47 +0200

    Fix phpstan errors

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -26,18 +26,24 @@ readonly class Config
      */
     public static function from(array $config): self
     {
+        /** @var string $basePath */
         $basePath = $config['basePath'] ?? throw InvalidConfigurationException::onMissingBasePath();
+        /** @var array<string> $default */
         $default = $config['default'] ?? [];
+        /** @var array<int, array<string, mixed>> $modules */
         $modules = $config['modules'] ?? throw InvalidConfigurationException::onMissingModules();
 
         $modules = array_map(
             function (array $moduleConfig) use ($basePath) {
+                /** @var string $module */
                 $module = $moduleConfig['module'] ?? throw InvalidConfigurationException::onMissingModule();
+                /** @var array<string> $exclude */
+                $exclude = $moduleConfig['exclude'] ?? [];
 
                 return new Module(
                     $module,
                     $basePath . self::PATH_SEPARATOR . $module,
-                    $moduleConfig['exclude'] ?? [],
+                    $exclude,
                 );
             },
             $modules,
