f15bac36f8315c73110dfb7ed1f650cec4d1417c
Jason Viljoen <21276558+jcvviljoen@users.noreply.github.com>
2024-08-13 22:58:16 +0200

    Update test to validate actual array

--- tests/Unit/Infrastructure/Console/ConsoleOutputWriterTest.php
+++ tests/Unit/Infrastructure/Console/ConsoleOutputWriterTest.php
@@ -6,6 +6,7 @@ namespace Stability\Tests\Unit\Infrastructure\Console;
 
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Stability\Component\ComponentResult;
 use Stability\Infrastructure\Console\ConsoleOutputWriter;
 use Stability\Tests\_Fixtures\StabilityResultFactory;
 use Symfony\Component\Console\Output\ConsoleOutput;
@@ -29,12 +30,45 @@ class ConsoleOutputWriterTest extends TestCase
     {
         $result = StabilityResultFactory::testSource();
         $expects = count($result->componentResults);
+        $expectedOutput = $this->generateExpectedOutput($result->componentResults);
 
         $this->consoleOutput
             ->expects($this->exactly($expects))
             ->method('writeln')
-            ->with($this->isType('array'));
+            ->willReturnCallback(
+                fn (array $output) => $this->assertContains($output, $expectedOutput),
+            );
 
         $this->consoleOutputWriter->outputResult($result);
+    }
+
+    /**
+     * @param array<ComponentResult> $componentResults
+     * @return array<string, string>
+     */
+    private function generateExpectedOutput(array $componentResults): array
+    {
+        $output = [];
+
+        foreach ($componentResults as $componentResult) {
+            $zoneDescription = $componentResult->zone->description();
+            $zoneIcon = $componentResult->zone->icon();
+
+            $resultOutput = [];
+
+            $resultOutput[] = '----------------------------------------';
+            $resultOutput[] = "Component: {$componentResult->component->module->name}";
+            $resultOutput[] = '----------------------------------------';
+            $resultOutput[] = "| Abstractness: $componentResult->abstractness";
+            $resultOutput[] = "| Instability: $componentResult->instability";
+            $resultOutput[] = "| DMS: $componentResult->dms";
+            $resultOutput[] = "| Zone: $zoneIcon $zoneDescription";
+            $resultOutput[] = '----------------------------------------';
+            $resultOutput[] = '';
+
+            $output[] = $resultOutput;
+        }
+
+        return $output;
     }
 }
