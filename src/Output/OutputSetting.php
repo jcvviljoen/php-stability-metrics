69f13f715fe25e20d01a71194346eb8e9d0cb5d3
Jason Viljoen <jcv.viljoen@gmail.com>
2026-01-04 21:21:00 +0100

    Set console as default output

--- src/Output/OutputSetting.php
+++ src/Output/OutputSetting.php
@@ -26,7 +26,7 @@ readonly class OutputSetting
     public static function default(): self
     {
         return new self(
-            OutputOption::JSON,
+            OutputOption::CONSOLE,
             '',
             self::DEFAULT_FILE_NAME,
         );
